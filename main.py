5a613394681c7024d8e2a6a332c67c15473acdf5
lgg-lg <lingganguligu@outlook.com>
2025-09-05 16:03:44 +0800

    Update main.py
    [update] add2module

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # 导入所有模块
-from modules import module1_capture, module2_combine, module3_clean, module4_split # 注意导入顺序
+from modules import module1_capture, module2_combine, module3_clean, module4_split, module5_pick,module6_result # 注意导入顺序
 import os
 
 if __name__ == '__main__':
@@ -16,7 +16,12 @@ if __name__ == '__main__':
 
     print("开始执行模块4：拆分信号源")
     # 注意：模块3现在应该读取清理后的文件
-    # 需要修改 module3_split.py 中读取的文件路径
     module4_split.split_channels()
+
+    print("开始执行模块5：优选频道信号源") # <<< 新增 >>>
+    module5_pick.main() # <<< 新增 >>>
+
+    print("开始执行模块6：替换用户频道列表") # <<< 新增 >>>
+    module6_result.main() # <<< 新增 >>>
 
     print("✅ 所有模块执行完成")
